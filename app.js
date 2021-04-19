b45ea25ac85073f72202c3f10c8b32204d182a36
luckybharat <bharatjpr007@gmail.com>
2021-04-19 10:16:09 +0530

    test update

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const PORT  = process.env.PORT || 3006;
 app.get("/", (req, res) => {
     return res.send("it worked yeah");
 })
+
+app.get("/login", (req, res) => {
+    return res.send("done with it");
+});
+
 app.listen(PORT, () => {
     console.log("app is running at http://localhost:"+PORT);
 });
